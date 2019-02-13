6c2bcb9355ebc799f78c2ee9ddcd261031b42b1a
Alexis Dewerdt <alexis.dewerdt@laposte.net>
2019-02-13 12:57:52 +0100

    models created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Reviews.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'italian'
+  },
+  {
+    name:         'Epicure',
+    address:      '75008 Paris',
+    category:     'french'
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+
+puts 'Creating reviews...'
+
+
+
+puts 'Finished!'
